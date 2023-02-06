6b2ccd3aca382de3450f0ecd62f573d25f867621
lazydt <projec.area@gmail.com>
2023-02-06 16:58:30 +0300

    added the solution file and library to project

--- VCSLibrary/Class1.cs
+++ VCSLibrary/Class1.cs
@@ -1,4 +1,5 @@
 ﻿namespace VCSLibrary;
+
 public class Class1
 {
 
--- VCSTests/UnitTest1.cs
+++ VCSTests/UnitTest1.cs
@@ -1,10 +1,11 @@
-namespace VCSTests;
-
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ReturnByteCollection()
     {
+
+
+
 
     }
 }
